834f2ba992e3dc35b43916acbddde92bf1237b1c
nadav144 <nadav@rotenberg.co.il>
2016-01-25 18:56:06 +0200

    impl. Distance calculator using Google Maps

--- Distance.js
+++ Distance.js
@@ -1,7 +1,7 @@
 /**
  * Created by Nadav on 25/01/2016.
  */
-function Distance(){
+function Distance(start, finish, time, airDistance) {
     this.start = {};
     this.finish = {};
     this.time = 0.0;
--- MapServices.js
+++ MapServices.js
@@ -6,6 +6,10 @@ function getPOIsAroundLocation (location, radius, preferences) {
     return [new POI()];
 }
 
-function getDistance(start, end){
-    return new Distance();
+function getDistance(start, finish) {
+    var distanceInMeters = google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(start.lat, start.lng), new google.maps.LatLng(finish.lat, finish.lng));
+    // distance --> km. / 60 km per hour
+    var time = ((distanceInMeters / 1000) / 60);
+    var drivingFactor = 1.3;
+    return new Distance(start, finish, time * drivingFactor, distanceInMeters);
 }
